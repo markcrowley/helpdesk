1cec38cb10b8705e9b59edf7add4c40d67691171
Mark Crowley <markcrowley@gmail.com>
2016-10-24 17:34:09 +0100

    Added details

--- app/controllers/Tickets.java
+++ app/controllers/Tickets.java
@@ -5,11 +5,14 @@ package controllers;
 
 import java.util.List;
 
+import com.google.inject.Inject;
 
 import models.Ticket;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.tickets.list;
+import views.html.tickets.details;
 /**
  * <p>
  * Tickets
@@ -19,13 +22,15 @@ import views.html.tickets.list;
  */
 public class Tickets extends Controller {
 	
+	@Inject FormFactory formFactory;
+	
 	public Result list() {
 		List<Ticket> tickets = Ticket.findAll();
 		return ok(list.render(tickets));
 	}
 
 	public Result newTicket() {
-		return TODO;
+		return ok(details.render(formFactory.form(Ticket.class)));
 	}
 
 	public Result details(Long id) {
